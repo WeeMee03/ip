a8f7f4cdce31011a67303f9856f02be2d979add3
WeeMee03 <kaneng00@gmail.com>
2025-08-28 20:26:09 +0800

    Initialize repo with starter code

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Elena User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Elena.java
@@ -0,0 +1,11 @@
+public class Elena {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Elena");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
